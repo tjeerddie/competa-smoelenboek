a3ee9dd403930f59fa4891ad58f877abab7e6978
tjeerddie <tjeerdverschragen@hotmail.com>
2016-08-29 14:26:27 +0200

    removed not needed db config

--- resources/config.php
+++ resources/config.php
@@ -2,29 +2,7 @@
 
 /*
     include data and settings that every page needs.
-    ex. when databe credentials change you only change it here.
 */
-
-$config = array(
-    "db" => array(
-        "Database" => array(
-            "dbname" => "smoelenboek",
-            "username" => "root",
-            "password" => "",
-            "host" => "localhost"
-        ),
-    ),
-    "urls" => array(
-        "baseUrl" => "http://example.com"
-    ),
-    "paths" => array(
-        "resources" => "/path/to/resources",
-        "images" => array(
-            "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
-            "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
-        )
-    )
-);
 
 /*
     Creating constants for heavily used paths.
