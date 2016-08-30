5653003e1205b61e17aac52985fc01e1dc25354b
RubeRuby <rubenvanderknaap@live.nl>
2016-08-30 08:41:57 +0200

    Added a search template

--- resources/view/templates/visitorPages/home.php
+++ resources/view/templates/visitorPages/home.php
@@ -1,5 +1,7 @@
 <?php
     require_once("resources/view/templates/header.php");
+
     require_once("resources/view/templates/search.php");
+    
     require_once("resources/view/templates/footer.php");
 ?>
