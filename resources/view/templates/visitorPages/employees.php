29184eab13f39b43869e41a2fa6ce2e46804f052
nigel <nigel@competa.com>
2016-09-02 14:17:51 +0200

    fixed the employees.

--- resources/view/templates/visitorPages/employees.php
+++ resources/view/templates/visitorPages/employees.php
@@ -22,7 +22,21 @@
           <img class="figure__image image--fluid card__image image--rounded" src="app/img/content/<?= $employee->getPhoto() ?>" alt="<?= $employee->getFullName() ?>" />
           <div class="divider__block divider__block--blue"></div>
           <figcaption class=" figure__caption card__block">
-            <h4 class="card__title"><?= $employee->getFirstName() ." " . $employee->getLastName() ?></h4>
+            <h4 class="card__title"><?= $employee->getfirstName() ?></h4>
+            <h4 class="card__title"><?= $employee->getlastName() ?></h4>
+          </figcaption>
+          <a class="card__overlay" href="#">
+            <p class="card__text">Click to see more about</p>
+          </a>
+        </figure>
+      <?php endforeach; ?>
+      <?php foreach ($employees as $employee): ?>
+        <figure class="figure grid__column-xl-3 card">
+          <img class="figure__image image--fluid card__image image--rounded" src="app/img/content/<?= $employee->getPhoto() ?>" alt="<?= $employee->getFullName() ?>" />
+          <div class="divider__block divider__block--blue"></div>
+          <figcaption class=" figure__caption card__block">
+            <h4 class="card__title"><?= $employee->getfirstName() ?></h4>
+            <h4 class="card__title"><?= $employee->getlastName() ?></h4>
           </figcaption>
           <a class="card__overlay" href="#">
             <p class="card__text">Click to see more about</p>
