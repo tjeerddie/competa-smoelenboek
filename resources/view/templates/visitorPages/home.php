dbcbe76a492ee239a1c9b11400bfae9cb0d02d26
ruberuby <rubenvanderknaap@live.nl>
2016-09-05 11:36:29 +0200

    Fixed homepage and login layout

--- resources/view/templates/visitorPages/home.php
+++ resources/view/templates/visitorPages/home.php
@@ -9,28 +9,28 @@
                     <span></span>
                 </button>
             </header>
-        <?php   require_once("resources/view/templates/search.php");
-            echo "Employees </br></br>";
-            foreach ($employees as $employee):?>
-            <a href="?control=Visitor&action=employee&id=<?php echo $employee->getId();?>">
-                <?php echo "name: " . $employee->getFirstName()." ".$employee->getMiddleName()." ".$employee->getLastName();?>
-            </a>
-            <?php echo "</br> email: " . $employee->getEmail();
-            echo "</br> phone: " . $employee->getPhoneNumber();
-            foreach ($groups as $group) {
-                if ($employee->getGroupId() === $group->getId()) {
-                    echo "</br> group: " . $group->getName();
-                }
-            }
-            foreach ($jobs as $job) {
-                if ($employee->getCategoryId() === $job->getId()) {
-                    echo "</br> job: " . $job->getType();
-                }
-            }
-            echo "</br></br>";
-        endforeach;
-        echo "</br></br>";
-        ?>
+            <section class="grid__container">
+              <header class="main__header">
+                <h1 class="main__heading">Home</h1>
+              </header>
+              <div class="divider">
+                <div class="divider__block divider__block--absolute divider__block--red"></div>
+              </div>
+              <p class="main__text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Integer sed mi maximus lectus volutpat efficitur fermentum et dolor.
+                Donec facilisis elementum scelerisque. Sed dignissim turpis quis lorem volutpat, et viverra nisi consectetur.
+                Aenean interdum ipsum sed nulla tempor malesuada. Mauris mauris odio, finibus non arcu a, malesuada tincidunt arcu.
+                Mauris porta consequat placerat.
+                Vestibulum fermentum sapien ac nibh mattis volutpat et ac justo.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed augue mi, dignissim ut arcu eu, tempor gravida turpis.
+                Quisque vitae nisi sit amet magna lobortis faucibus sed id sapien. In massa orci, volutpat eu elementum nec, ullamcorper in lacus.
+                Aliquam cursus nibh in congue maximus. Suspendisse potenti. Vivamus fermentum arcu turpis, ac dapibus tellus ornare vel. In sed vulputate quam. Nunc consectetur sollicitudin metus a commodo. Fusce egestas sed nibh eu pulvinar. Nullam gravida orci varius sapien dictum, et volutpat risus fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse potenti.
+              </p>
+            </section>
+
+
+
         </main>
 
 
