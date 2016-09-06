fb1ed3d5fae4ef979da707fb22400a2a8df46426
tjeerddie <tjeerdverschragen@hotmail.com>
2016-09-06 09:12:27 +0200

    styled employee

--- resources/view/templates/visitorPages/employee.php
+++ resources/view/templates/visitorPages/employee.php
@@ -10,58 +10,68 @@
                     <div class="divider__block divider__block--absolute divider__block--red"></div>
                 </div>
                 <div class="grid__row employee__box">
-                    <div class="grid__column-xl-5 grid__column--offset-xl-1">
-                        <figure class="figure">
-                            <img class="employee--image" src="app/img/content/<?= $employee->getPhoto();?>" alt="de photo of <?= $employee->getFullName();?>"></img>
-                        </figure>
-                    </div>
-                    <div class="grid__column-xl-5">
-                        <ul class="list--reset employee__list">
-                            <li class="employee__list-item">
-                                <div class="employee__container">
-                                    <h5 class="employee__text">Name: <?= $employee->getFullName();?></h5>
-                                    <h5 class="employee__info"></h5>
-                                </div>
-                            </li>
-                            <li class="employee__list-item">
-                                <div class="employee__container">
+                    <figure class="figure grid__column-xl-3 grid__column--offset-xl-1">
+                        <img class="figure__image image--fluid employee--image" src="app/img/content/<?= $employee->getPhoto();?>" alt="de photo of <?= $employee->getFullName();?>"></img>
+                    </figure>
+                    <div class="grid__column-xl-6">
+                        <table class="list--reset employee__list">
+                            <tr class="employee__list-item table__row">
+                                <td class="table__cell">
+                                    <h5 class="employee__text">Name: </h5>
+                                </td>
+                                <td class="table__cell">
+                                    <h5 class="employee__info"> <?= $employee->getFullName();?></h5>
+                                </td>
+                            </tr>
+                            <tr class="employee__list-item table__row">
+                                <td class="table__cell">
                                     <h5 class="employee__text">Email: </h5>
+                                </td>
+                                <td class="table__cell">
                                     <h5 class="employee__info"><?= $employee->getEmail();?></h5>
-                                </div>
-                            </li>
-                            <li class="employee__list-item">
-                                <div class="employee__container">
+                                </td>
+                            </tr>
+                            <tr class="employee__list-item table__row">
+                                <td class="table__cell">
                                     <h5 class="employee__text">Phone: </h5>
+                                </td>
+                                <td class="table__cell">
                                     <h5 class="employee__info"><?= $employee->getPhoneNumber();?></h5>
-                                </div>
-                            </li>
-                            <li class="employee__list-item">
-                                <div class="employee__container">
+                                </td>
+                            </tr>
+                            <tr class="employee__list-item table__row">
+                                <td class="table__cell">
                                     <h5 class="employee__text">Group: </h5>
+                                </td>
+                                <td class="table__cell">
                                     <?php foreach ($groups as $group) :
                                         if ($employee->getGroupId() === $group->getId()) :?>
                                             <h5 class="employee__info"><?= $group->getName();?></h5>
                                         <?php endif;
                                     endforeach;?>
-                                </div>
-                            </li>
-                            <li class="employee__list-item">
-                                <div class="employee__container">
+                                </td>
+                            </tr>
+                            <tr class="employee__list-item table__row">
+                                <td class="table__cell">
                                     <h5 class="employee__text">Job: </h5>
+                                </td>
+                                <td class="table__cell">
                                     <?php foreach ($jobs as $job) :
                                         if ($employee->getCategoryId() === $job->getId()) :?>
                                             <h5 class="employee__info"><?= $job->getType();?></h5>
                                         <?php endif;
                                     endforeach;?>
-                                </div>
-                            </li>
-                            <li class="employee__list-item">
-                                <div class="employee__container">
+                                </td>
+                            </tr>
+                            <tr class="employee__list-item table__row">
+                                <td class="table__cell">
                                     <h5 class="employee__text">Description: </h5>
+                                </td>
+                                <td class="table__cell">
                                     <h5 class="employee__info"><?= $employee->getDescription();?></h5>
-                                </div>
-                            </li>
-                        </ul>
+                                </td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>
