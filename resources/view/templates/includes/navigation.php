f61a18873d00e1d6b1997e7be688aba4a09f4b97
tjeerddie <tjeerdverschragen@hotmail.com>
2016-09-08 11:24:23 +0200

    added the is-active for the navigation

--- resources/view/templates/includes/navigation.php
+++ resources/view/templates/includes/navigation.php
@@ -6,7 +6,7 @@
   <ul class="navigation__list-item list--reset">
     <li class="navigation__list-item" role="button">
       <a class="navigation__link" href="?control=<?= $control?>&action=home">
-        <div class="navigation__link__wrapper is-active">
+        <div class="navigation__link__wrapper <?= $action==="home"?"is-active":"" ?>">
           <p class="navigation__link__title">home</p>
           <span class="fa fa-home navigation__link__icon" role="button"></span>
         </div>
@@ -14,7 +14,7 @@
     </li>
     <li class="navigation__list-item">
       <a class="navigation__link" href="?control=<?= $control?>&action=employees">
-        <div class="navigation__link__wrapper">
+        <div class="navigation__link__wrapper <?= $action==="employees"?"is-active":"" ?>">
           <p class="navigation__link__title">employees</p>
           <span class="fa fa-users navigation__link__icon" role="button"></span>
         </div>
@@ -22,7 +22,7 @@
     </li>
     <li class="navigation__list-item">
       <a class="navigation__link" href="?control=<?= $control?>&action=<?= $control==="User"? "logout" : "login"?>">
-        <div class="navigation__link__wrapper">
+        <div class="navigation__link__wrapper <?= $action==="login"?"is-active":"" ?>">
           <p class="navigation__link__title"><?= !isset($_SESSION['user'])? "Sign in" : "Sign out"?></p>
           <span class="fa fa-sign-<?= $control==="User"? "out" : "in"?> navigation__link__icon" role="button"></span>
         </div>
