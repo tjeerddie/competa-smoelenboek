b33ae92688fc72c53a2978dd893621fc168f3788
tjeerddie <tjeerdverschragen@hotmail.com>
2016-09-12 13:16:18 +0200

    removed the add button at visitor

--- resources/view/templates/includes/search.php
+++ resources/view/templates/includes/search.php
@@ -1,8 +1,10 @@
 <div class="sub-menu">
   <form class="sub-menu--left"  method="post" id="form">
+    <?php if($action === 'User') :?>
     <a class="button button--primary sub-menu--button" href="?control=User&action=addEmployee">
       <i class="fa fa-user-plus sub-menu--icon button--search"></i>
     </a>
+  <?php endif;?>
     <input class="form__control form__control--search"  type="text" name="name">
     <button class="button button--primary button--search" type="submit" name="search" role="button">
       <i class="fa fa-search"></i>
