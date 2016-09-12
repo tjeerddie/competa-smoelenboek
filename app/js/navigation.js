f4d158cc99891a4b28b7df0a10dbb1fdf85e3039
nhoegee <nigel@competa.com>
2016-09-12 08:51:46 +0200

    Added responsiveness for the mobile navigation version.

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -9,6 +9,34 @@ $(document).ready(function () {
   var $navigationLinkTitle = $('.navigation__link__title');
   var $navigationHamburger = $('.navigation__hamburger');
 
+  var add = function () {
+    $header.addClass('is-enlarged');
+    $main.addClass('is-enlarged');
+    $navigationHamburger.addClass('is-open');
+    $navigation.addClass('is-ensmalled');
+    $navigationListItem.addClass('is-ensmalled');
+    $navigationImage.addClass('is-ensmalled');
+    $navigationLinkTitle.addClass('is-ensmalled');
+  }
+
+  if ($(window).width() <= 544){
+    add();
+  }
+
+  $(window).resize(function(){
+     if ($(window).width() <= 544){
+       add();
+     } else {
+       $header.removeClass('is-enlarged');
+       $main.removeClass('is-enlarged');
+       $navigationHamburger.removeClass('is-open');
+       $navigation.removeClass('is-ensmalled');
+       $navigationListItem.removeClass('is-ensmalled');
+       $navigationImage.removeClass('is-ensmalled');
+       $navigationLinkTitle.removeClass('is-ensmalled');
+     }
+   });
+
   $navigationHamburger.on('click', function (e) {
     $header.toggleClass('is-enlarged');
     $main.toggleClass('is-enlarged');
