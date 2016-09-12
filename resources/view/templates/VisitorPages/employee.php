953726b958a792d51bfa84ad3213f8cfa0ae4e6c
nhoegee <nigel@competa.com>
2016-09-12 12:45:51 +0200

    fixed show employee

--- resources/view/templates/VisitorPages/employee.php
+++ resources/view/templates/VisitorPages/employee.php
@@ -1,81 +1,52 @@
 <?php require_once(INCLUDES_PATH . "header.php");
     require_once(INCLUDES_PATH . "navigation.php");?>
-        <main class="main">
-            <?php require_once(INCLUDES_PATH . "hamburger.php");?>
-            <div class="grid__container employee">
-                <header class="main__header">
-                    <h1 class="main__heading"><?= $employee->getFullName();?></h1>
-                </header>
-                <div class="divider">
-                    <div class="divider__block divider__block--absolute divider__block--red"></div>
-                </div>
-                <div class="grid__row employee__box">
-                    <figure class="figure grid__column-xl-3 grid__column--offset-xl-1">
-                        <img class="figure__image image--fluid employee--image" src="app/img/content/<?= $employee->getPhoto();?>" alt="de photo of <?= $employee->getFullName();?>"></img>
-                    </figure>
-                    <div class="grid__column-xl-6">
-                        <table class="list--reset employee__list">
-                            <tr class="employee__list-item table__row">
-                                <td class="table__cell">
-                                    <h5 class="employee__text">Name: </h5>
-                                </td>
-                                <td class="table__cell">
-                                    <h5 class="employee__info"> <?= $employee->getFullName();?></h5>
-                                </td>
-                            </tr>
-                            <tr class="employee__list-item table__row">
-                                <td class="table__cell">
-                                    <h5 class="employee__text">Email: </h5>
-                                </td>
-                                <td class="table__cell">
-                                    <h5 class="employee__info"><?= $employee->getEmail();?></h5>
-                                </td>
-                            </tr>
-                            <tr class="employee__list-item table__row">
-                                <td class="table__cell">
-                                    <h5 class="employee__text">Phone: </h5>
-                                </td>
-                                <td class="table__cell">
-                                    <h5 class="employee__info"><?= $employee->getPhoneNumber();?></h5>
-                                </td>
-                            </tr>
-                            <tr class="employee__list-item table__row">
-                                <td class="table__cell">
-                                    <h5 class="employee__text">Group: </h5>
-                                </td>
-                                <td class="table__cell">
-                                    <?php foreach ($groups as $group) :
-                                        if ($employee->getGroupId() === $group->getId()) :?>
-                                            <h5 class="employee__info"><?= $group->getName();?></h5>
-                                        <?php endif;
-                                    endforeach;?>
-                                </td>
-                            </tr>
-                            <tr class="employee__list-item table__row">
-                                <td class="table__cell">
-                                    <h5 class="employee__text">Job: </h5>
-                                </td>
-                                <td class="table__cell">
-                                    <?php foreach ($jobs as $job) :
-                                        if ($employee->getCategoryId() === $job->getId()) :?>
-                                            <h5 class="employee__info"><?= $job->getType();?></h5>
-                                        <?php endif;
-                                    endforeach;?>
-                                </td>
-                            </tr>
-                            <tr class="employee__list-item table__row">
-                                <td class="table__cell">
-                                    <h5 class="employee__text">Description: </h5>
-                                </td>
-                                <td class="table__cell">
-                                    <h5 class="employee__info"><?= $employee->getDescription();?></h5>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
+    <main class="main main--employee">
+        <?php require_once(INCLUDES_PATH . "hamburger.php");?>
+        <div class="grid__container">
+          <form method="post" enctype="multipart/form-data" autocomplete="off">
+            <div class="form__group grid__row">
+              <figure class="figure form__logo form__logo--photo grid__column-lg-5">
+                <img class="figure__image form__image employee--image" src="app/img/content/<?= $employee->getPhoto();?>" alt="Photo of <?= $employee->getFullName();?>" />
+              </figure>
+              <div class="form__group grid__column-lg-7 grid__row">
+                <span class="form__label grid__column-lg-4">first name</span>
+                <span class="form__control form__control--no-border grid__column-lg-8"><?= $employee->getFirstName();?></span>
+              </div>
+              <div class="form__group grid__column-lg-7 grid__row">
+                <span class="form__label grid__column-lg-4">last name</span>
+                <span class="form__control form__control--no-border grid__column-lg-8"><?= $employee->getLastName();?></span>
+              </div>
+              <div class="form__group grid__column-lg-7 grid__row">
+                <span class="form__label grid__column-lg-4">email</span>
+                <span class="form__control form__control--no-border grid__column-lg-8"><?= $employee->getEmail();?></span>
+              </div>
+              <div class="form__group grid__column-lg-7 grid__row">
+                <span class="form__label grid__column-lg-4">phone</span>
+                <span class="form__control form__control--no-border grid__column-lg-8"><?= $employee->getPhoneNumber();?></span>
+              </div>
+              <div class="form__group grid__row form__group--lastRows grid__column-lg-5">
+                <span class="form__label grid__column-lg-4" for="inputJob">job</span>
+                <?php foreach ($jobs as $job) :
+                    if ($employee->getCategoryId() === $job->getId()) :?>
+                        <span class="form__control form__control--no-border grid__column-lg-8"><?= $job->getType();?></span>
+                    <?php endif;
+                endforeach;?>
+              </div>
+              <div class="form__group grid__column-lg-7 grid__row">
+                <span class="form__label grid__column-lg-4">group</span>
+                  <?php foreach ($groups as $group) :
+                      if ($employee->getGroupId() === $group->getId()) :?>
+                          <span class="form__control form__control--no-border grid__column-lg-8"><?= $group->getName();?></span>
+                      <?php endif;
+                  endforeach;?>
+              </div>
+              <div class="form__group grid__column-xs-12 grid__row">
+                <span class="form__label grid__column-lg-2">description</span>
+                <span class="form-control form__control--textarea grid__column-lg-10"><?= $employee->getDescription(); ?></span>
+              </div>
             </div>
-        </main>
-
-
+          </form>
+          <p class="employee__message"><?= isset($message) ? $message : ""?></p>
+        </div>
+    </main>
 <?php    require_once(INCLUDES_PATH . "footer.php");?>
