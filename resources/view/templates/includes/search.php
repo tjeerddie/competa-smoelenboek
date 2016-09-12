a3aabe0e0a342e6349430cd9451a80a27b7c30b3
tjeerddie <tjeerdverschragen@hotmail.com>
2016-09-12 13:18:18 +0200

    fixed add button

--- resources/view/templates/includes/search.php
+++ resources/view/templates/includes/search.php
@@ -1,6 +1,6 @@
 <div class="sub-menu">
   <form class="sub-menu--left"  method="post" id="form">
-    <?php if($action === 'User') :?>
+    <?php if($control === 'User') :?>
     <a class="button button--primary sub-menu--button" href="?control=User&action=addEmployee">
       <i class="fa fa-user-plus sub-menu--icon button--search"></i>
     </a>
